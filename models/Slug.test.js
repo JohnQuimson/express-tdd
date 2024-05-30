4f98aeab8ea539da91cb1e61c77934da7ebcd6f1
JohnQuimson <john.quimson777@gmail.com>
2024-05-30 15:22:12 +0200

    fix: return string

--- models/Slug.test.js
+++ models/Slug.test.js
@@ -1,1 +1,30 @@
+// importazione non necessaria, ma usata per intellisense
 const { test, expect } = require('@jest/globals');
+const fs = require('fs');
+const path = require('path');
+
+const getSlug = () => {
+  const filePath = path.resolve(__dirname, '../db/posts.json');
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const posts = JSON.parse(fileContent);
+  return posts.map((post) => post.slug);
+};
+
+const createSlug = (string) => {
+  const array = getSlug();
+  let originalSlug = string.toLowerCase().replaceAll(' ', '-');
+  let newSlug = originalSlug;
+  // counter per garantire l'unicità dello slug
+  let counter = 1;
+  while (array.includes(newSlug)) {
+    newSlug = `${originalSlug}-${counter}`;
+    counter++;
+  }
+  return newSlug;
+};
+
+// return stringa
+test('createSlug dovrebbe ritornare una stringa', () => {
+  const slug = createSlug('titolo di esempio');
+  expect(typeof slug).toBe('string');
+});
